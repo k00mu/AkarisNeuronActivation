e4df00bebbf5e1615d10d037f09dcffaeb1e4a00
Atqa Munzir <atqamz@gmail.com>
2023-02-05 16:16:41 +0700

    temp lagi

--- Assets/_Project/_Scripts/NerveSystem.cs
+++ Assets/_Project/_Scripts/NerveSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Akari
 {
@@ -92,6 +93,14 @@ namespace Akari
             {
                 timer.StopTimer();
                 GameOver = true;
+            }
+
+            if (GameOver)
+            {
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
             }
         }
 
